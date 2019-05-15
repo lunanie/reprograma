7d23fa8301add0bdb64f0bd0851805afc167463c
unknown <lunanie@gmail.com>
2019-05-15 15:53:05 -0300

    tarot

---
+++ semana10/T7-jquery-Aula3/03/AJAX/arquivo/js/script.js
@@ -0,0 +1,24 @@
+function carregaCarta() {
+    $.getJSON("tarot.json", trocaCartaAleatoria);
+}
+
+function trocaCartaAleatoria(cartas, status) {
+    if (status == 'success') {
+
+        let numeroAleatorio = Math.floor(Math.random() * cartas.length);
+        $(".titulo").text(cartas[numeroAleatorio].nome);
+        $(".tipo").text(cartas[numeroAleatorio].tipo);
+        $(".descricao").text(cartas[numeroAleatorio].descricao);
+        $(".image").attr("src", cartas[numeroAleatorio].imagem);
+        $(".saiba-mais").attr("src", cartas[numeroAleatorio].link);
+
+    } else {
+        $(".mensagem").text("Não foi possível carregar a carta");
+    }
+}
+
+$(document).ready(function() {
+    carregaCarta();
+    $("#novaCarta").click(carregaCarta);
+
+});
