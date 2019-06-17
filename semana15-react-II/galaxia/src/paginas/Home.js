83e66862ed9c7b12cfff6eb30dd540053c7c4294
unknown <lunanie@gmail.com>
2019-06-17 14:45:24 -0300

    galaxia

---
+++ semana15-react-II/galaxia/src/paginas/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+export default function Home (props) {
+    return(
+        <p>Você está na Home</p>
+        )
+    }
+    
+    // function Home (props) {
+    //     return(
+    //         <p>Você está na Home</p>
+    //     )
+    // }
+    
+    // export default Home
