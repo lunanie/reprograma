f689e88f9d38a64fc9951597a9ee18b691ad32d0
unknown <lunanie@gmail.com>
2019-06-17 14:59:35 -0300

    gitignore

--- semana15-react-II/galaxia/src/paginas/Home.js
+++ semana15-react-II/galaxia/src/paginas/Home.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import './home.css'
 export default function Home (props) {
     return(
-        <p>Você está na Home</p>
+        <section className="home-header">
+            <h1>
+                A era da comunicação intergalática
+            </h1>
+
+        </section>
         )
     }
     
